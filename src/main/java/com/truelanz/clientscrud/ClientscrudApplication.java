665e0b5035a58d733afc4acede01a9ed053efe75
truelanz <devalanoliveira@gmail.com>
2023-06-23 12:34:52 -0300

    first commit

---
+++ src/main/java/com/truelanz/clientscrud/ClientscrudApplication.java
@@ -0,0 +1,13 @@
+package com.truelanz.clientscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientscrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientscrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/truelanz/clientscrud/ClientscrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truelanz.clientscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientscrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
